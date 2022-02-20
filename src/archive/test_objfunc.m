460f40a3318706ad3f731003b35a8ae135246024
mikeaalv <yue.wu@uga.edu>
2022-02-20 13:52:41 -0500

    del archive files

--- src/archive/test_objfunc.m
+++
@@ -1,7 +0,0 @@
-function obj=test_objfunc(x,timevec,resvec,simui)
-  simusig=zeros(size(timevec));
-  for i=1:simui
-    shifti=(i-1)*3;
-    simusig=simusig+(x(3+shifti).*sin(2*pi*x(1+shifti)*timevec)).*exp(-x(2+shifti)*timevec);
-  end
-  obj= -log(sum((simusig-resvec).^2));
