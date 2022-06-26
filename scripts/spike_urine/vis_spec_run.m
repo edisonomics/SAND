4a8d6d9e7bc8782ba0f56d708c2993bb29100d47
mikeaalv <yue.wu@uga.edu>
2022-06-26 14:29:18 -0400

    Update vis_spec_run.m

--- scripts/spike_urine/vis_spec_run.m
+++ scripts/spike_urine/vis_spec_run.m
@@ -109,7 +109,7 @@ quan_str.integral=est_other_tab(:,{'PPM','lambda','integral','phase','simulation
 save('data_temp.mat')
 % match peaks from different samples
 % example visualization regions
-exampreg=[0.965 1.047; 1.457 1.495; 1.86 1.94; 3.43 3.54; 3.58 3.63; 4.61 4.67; 5.2 5.3];
+exampreg=[0.965 1.047; 1.457 1.495; 1.73 1.774; 4.61 4.67; 5.2 5.3; 6.887 6.917; 7.339 7.353; 7.851 7.869];
 % peak match for spike in peaks [ref bin, mixture bin]
 ppmref_match=[0.97 0.98 0.97 0.98; 0.983 0.99 0.983 0.99; 1.02 1.03 1.02 1.03; 1.035 1.042 1.035 1.042; 1.468 1.473 1.472 1.477; 1.48 1.485 1.484 1.489; 1.733 1.746 1.733 1.746; 1.746 1.761 1.746 1.761; 1.761 1.773 1.761 1.773; 1.895 1.902 1.897 1.904; 3.373 3.38 3.383 3.388; 3.389 3.396 3.399 3.405; 3.593 3.599 3.605 3.609; 3.601 3.607 3.612 3.616; 4.624 4.637 4.633 4.645; 4.637 4.649 4.648 4.658; 5.218 5.224 5.228 5.234; 5.224 5.231 5.234 5.241; 6.89 6.902 6.927 6.939; 6.904 6.913 6.943 6.953; 7.34 7.351 7.371 7.382; 7.853 7.868 7.87 7.883; 3.478 3.484 3.487 3.494];
 % ppmref_hard=[3.218 3.226 3.228 3.232; 3.237 3.24 3.244 3.249; 3.248 3.254 3.257 3.26; 3.406 3.411 3.414 3.419];
@@ -121,6 +121,7 @@ vol_add=[0,20,40,60,80,20];%the last one is reference
 sampnames={'0 (urine)','20','40','60','80','ref'};
 nsample=6;
 sampi_interest=[1:nsample];
+sampi_spike=[1:(nsample-1)];
 dssrange=[-0.5 0.5];
 refspec=ft_mat(nsample,:);
 normpeak=[3.034 3.043];
@@ -158,7 +159,7 @@ for quanmethod=quan_methods
   for ppair_i=1:npairs
     locppm=ppmpairtab(ppair_i,:);
     if labels(ppair_i)==1
-      searchppm_reg=[repmat(locppm(3:4),[5,1]); locppm(1:2)];
+      searchppm_reg=[repmat(locppm(3:4),[nsample-1,1]); locppm(1:2)];
     else
       searchppm_reg=[locppm(1:2); repmat(locppm(3:4),[4,1]); nan nan];
     end
@@ -182,10 +183,10 @@ for quanmethod=quan_methods
     end
     quan_coll=[quan_coll; tempvec];
     % scatter for each pair
-    locxvec=1:nsample;
+    locxvec=sampi_interest;
     locyvec=tempvec;
     h=figure();
-    spikeind=1:(nsample-1);
+    spikeind=sampi_spike;
     dlm=fitlm(locxvec(spikeind),locyvec(spikeind));
     lineestpara=dlm.Coefficients.Estimate';
     gscatter(locxvec,locyvec,[],[],[],[20]);
@@ -227,10 +228,10 @@ for quanmethod=quan_methods
     saveas(fig,['stack_example_region_sele' quanmethod num2str(regi) '.fig']);
     close all;
     % line plot for the regions
-    ind_reg=find(all(ppmpairtab>ppm_sele_range(1) & ppmpairtab<ppm_sele_range(2),2));
+    ind_reg=find(all(ppmpairtab(:,1:2)>ppm_sele_range(1) & ppmpairtab(:,1:2)<ppm_sele_range(2),2));
     quan_coll_loc=quan_coll(ind_reg,:);
     %
-    locxvec=repelem([1:nsample],length(ind_reg));
+    locxvec=repelem(sampi_interest,length(ind_reg));
     locyvec=quan_coll_loc(:);
     pairsize_loc=length(find(ind_reg<=pairsize(1)));
     pairsize_loc=[pairsize_loc length(ind_reg)-pairsize_loc(1)];
@@ -238,27 +239,34 @@ for quanmethod=quan_methods
     % plot
     h=figure();
     linecoll=[];
-    x_spikein=repelem([1:(nsample-1)],length(ind_reg));
-    y_spikein=quan_coll_loc(:,1:5);
+    x_spikein=repelem(sampi_spike,length(ind_reg));
+    y_spikein=quan_coll_loc(:,sampi_spike);
     y_spikein=y_spikein(:);
     color_spikein=repmat([repmat({'spike'},[pairsize_loc(1),1]);repmat({'urine'},[pairsize_loc(2),1])],[nsample-1,1])';
-    for color_group=unique(color_spikein)
-      groupind=find(cellfun(@(x) strcmp(x,color_group),color_spikein));
-      % linear line
-      dlm=fitlm(x_spikein(groupind),y_spikein(groupind));
-      % % plot the line
-      % subh=plot(dlm);
-      % delete(subh([1,3,4]));
+    % lines by spike-in or urine
+    % for color_group=unique(color_spikein)
+    %   groupind=find(cellfun(@(x) strcmp(x,color_group),color_spikein));
+    %   % linear line
+    %   dlm=fitlm(x_spikein(groupind),y_spikein(groupind));
+    %   % % plot the line
+    %   % subh=plot(dlm);
+    %   % delete(subh([1,3,4]));
+    %   linecoll=[linecoll; dlm.Coefficients.Estimate'];
+    % end
+    % lines by each peak
+    for rowi=1:size(quan_coll_loc,1)
+      seqind=sampi_spike;
+      dlm=fitlm(seqind,quan_coll_loc(rowi,seqind));
       linecoll=[linecoll; dlm.Coefficients.Estimate'];
     end
     % plot points
     gscatter(locxvec,locyvec,colorvec,[],[],[20]);
     for linei=1:size(linecoll,1)
-      refline(linecoll(linei,2),linecoll(linei,1));
+      hline=refline(linecoll(linei,2),linecoll(linei,1));
     end
     xlabel('spike volumn');
     ylabel('estimation');
-    title(['quantificaiton estimation']);
+    title(['quantificaiton estimation' num2str(ppm_sele_range(1)) ' ' num2str(ppm_sele_range(2))]);
     set(gca,'XTick',1:6,'XTickLabel',sampnames);
     saveas(h,['scatter_eval.' quanmethod num2str(regi) '_linepoints.fig']);
     close(h);
@@ -266,7 +274,7 @@ for quanmethod=quan_methods
   % scale each features (sample 3 spike 2 set as 2)
   quan_coll_s2=quan_coll./quan_coll(:,3)*2;
   % scatter plot
-  locxvec=repelem([1:nsample],npairs);
+  locxvec=repelem(sampi_interest,npairs);
   locyvec=quan_coll_s2(:);
   colorvec=repmat([repmat({'spike'},[pairsize(1),1]);repmat({'urine'},[pairsize(2),1])],[nsample,1]);
   %
@@ -280,11 +288,11 @@ for quanmethod=quan_methods
   close(h);
   % scale by range
   quan_coll_shift=quan_coll;
-  quan_coll_shift(:,1:5)=quan_coll_shift(:,1:5)-quan_coll_shift(:,1);
-  quan_coll_s_scale=quan_coll_shift./quan_coll_shift(:,5)*4;
+  quan_coll_shift(:,sampi_spike)=quan_coll_shift(:,sampi_spike)-quan_coll_shift(:,1);
+  quan_coll_s_scale=quan_coll_shift./quan_coll_shift(:,(nsample-1))*4;
   quan_coll_s_scale=quan_coll_s_scale(1:pairsize(1),:);
   % scatter plot
-  locxvec=repelem([1:nsample],pairsize(1));
+  locxvec=repelem(sampi_interest,pairsize(1));
   locyvec=quan_coll_s_scale(:);
   colorvec=repmat([repmat({'spike'},[pairsize(1),1])],[nsample,1]);
   %
@@ -298,7 +306,7 @@ for quanmethod=quan_methods
   close(h);
   % plot with linear lines
   seleseq=1:pairsize(1);
-  [intensityscale ordind]=sort(quan_coll(seleseq,5));
+  [intensityscale ordind]=sort(quan_coll(seleseq,nsample-1));
   cmap=jet(length(intensityscale));
   cinfo=customColormap(intensityscale,'colors',cmap(1:end,:));
   cmap=cinfo.rgb;
@@ -308,10 +316,10 @@ for quanmethod=quan_methods
   hold on;
   for rowindi=1:length(ordind)
     rowind=ordind(rowindi);
-    locxvec=[1:nsample];
+    locxvec=sampi_interest;
     locyvec=quan_coll(rowind,:);
     gscatter(locxvec,locyvec,[],cmap(rowindi,:),[],[20]);
-    groupind=1:5;
+    groupind=sampi_spike;
     % linear line
     dlm=fitlm(locxvec(groupind),locyvec(groupind));
     para=dlm.Coefficients.Estimate'
@@ -322,7 +330,7 @@ for quanmethod=quan_methods
   xlabel('spike volumn');
   ylabel('estimation');
   title(['quantificaiton estimation']);
-  set(gca,'XTick',1:6,'XTickLabel',sampnames);
+  set(gca,'XTick',sampi_interest,'XTickLabel',sampnames);
   % make a separate colorbar
   colormap(cinfo.cmap);
   t=colorbar;
