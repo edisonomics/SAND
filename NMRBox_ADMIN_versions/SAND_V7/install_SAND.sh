6d1fdd5173322b2392a93ca94e4f43e14b7986b0
Gerard Weatherby <gweatherby@uchc.edu>
2022-09-30 10:55:41 -0400

    add missing files to copy

--- NMRBox_ADMIN_versions/SAND_V7/install_SAND.sh
+++ NMRBox_ADMIN_versions/SAND_V7/install_SAND.sh
@@ -3,17 +3,18 @@
 ORIGIN=$(dirname $(readlink -f $0))
 #This software is provided "as is" with no warranties of any kind,  and without liability for use or loss of use.
 
-svn export https://github.com/edisonomics/SAND/trunk/scripts/NMRBox/pipe_scripts
-svn export https://github.com/edisonomics/SAND/trunk/src
+svn -q export https://github.com/edisonomics/SAND/trunk/scripts/NMRBox/pipe_scripts
+svn -q export https://github.com/edisonomics/SAND/trunk/src
 mkdir -p Edison_Lab_Shared_Metabolomics_UGA
 (
 	cd Edison_Lab_Shared_Metabolomics_UGA
-	svn export https://github.com/artedison/Edison_Lab_Shared_Metabolomics_UGA/trunk/metabolomics_toolbox
+	svn -q export https://github.com/artedison/Edison_Lab_Shared_Metabolomics_UGA/trunk/metabolomics_toolbox
 )
 
 #mkdir data temp log submissions data_preprocessed
 cp $ORIGIN/PRESAND.sh .
 cp $ORIGIN/SAND.sh .
+cp $ORIGIN/*m $ORIGIN/*txt .
 #chmod a+rx ./SUBMITSAND.sh
 
 #export PATH=$PATH:/home/nmrbox/osanati/read-only/test1/
